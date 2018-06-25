64b720bbf91cfafc66d42337d68d6e7f41d144f2
Pep Salcedo <salcedo.nerv@gmail.com>
2018-06-24 21:34:29 -0500

    Proyecto vacio

---
+++ iOSCourse/iOSCourse/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  iOSCourse
+//
+//  Created by Jose Martin Salcedo Lazaro on 24/06/18.
+//  Copyright © 2018 Jose Martin Salcedo Lazaro. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+  override func viewDidLoad() {
+    super.viewDidLoad()
+    // Do any additional setup after loading the view, typically from a nib.
+  }
+
+  override func didReceiveMemoryWarning() {
+    super.didReceiveMemoryWarning()
+    // Dispose of any resources that can be recreated.
+  }
+
+
+}
+
