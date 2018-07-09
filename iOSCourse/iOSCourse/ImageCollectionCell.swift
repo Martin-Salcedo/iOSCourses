19794d156e59087208098d7700c01af8432e3783
Pep Salcedo <salcedo.nerv@gmail.com>
2018-07-08 22:35:05 -0500

    se agregaron Imagenes y texto al collectionView

---
+++ iOSCourse/iOSCourse/ImageCollectionCell.swift
@@ -0,0 +1,16 @@
+//
+//  ImageCollectionCell.swift
+//  iOSCourse
+//
+//  Created by Jose Martin Salcedo Lazaro on 08/07/18.
+//  Copyright © 2018 Jose Martin Salcedo Lazaro. All rights reserved.
+//
+
+import UIKit
+
+class ImageCollectionCell: UICollectionViewCell {
+    
+  @IBOutlet weak var imageCell: UIImageView!
+  @IBOutlet weak var Textlbl: UILabel!
+  
+}
--- iOSCourse/iOSCourse/ViewController.swift
+++ iOSCourse/iOSCourse/ViewController.swift
@@ -10,6 +10,8 @@ import UIKit
 
 class ViewController: UIViewController {
   
+  var comida: [String] = ["camaron", "pizza", "hotdog", "tacos", "hamburgesa"]
+  
   override func viewDidLoad() {
     super.viewDidLoad()
     // Do any additional setup after loading the view, typically from a nib.
@@ -31,30 +33,29 @@ extension ViewController: UICollectionViewDelegate {
   }
   
   func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
-    let cell = collectionView.dequeueReusableCell(withReuseIdentifier: "cell", for: indexPath)
-    
-    let red = CGFloat(Int(arc4random_uniform(155)) + 100)
-    let blue = CGFloat(Int(arc4random_uniform(155)) + 100)
-    let green = CGFloat(Int(arc4random_uniform(155)) + 100)
-    
-    
-    cell.backgroundColor = UIColor(red: red/255, green: green/255, blue: blue/255, alpha: 1.0)
-//    cell.backgroundColor = UIColor.blue
+    let cell = collectionView.dequeueReusableCell(withReuseIdentifier: "cell", for: indexPath) as! ImageCollectionCell
+    cell.layer.cornerRadius = 10
+    cell.imageCell.image = UIImage(named: comida[indexPath.row])
+    cell.Textlbl.text = comida[indexPath.row]
     return cell
   }
 }
 
 extension ViewController: UICollectionViewDataSource {
   func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
-    return 100
+    return 5
   }
 }
 
 extension ViewController: UICollectionViewDelegateFlowLayout {
-//  func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, sizeForItemAt indexPath: IndexPath) -> CGSize {
-//    let screen = UIScreen.main.bounds
-////    return CGSize(width: screen.width, height: screen.height)
-//  }
+  func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, sizeForItemAt indexPath: IndexPath) -> CGSize {
+    let screen = UIScreen.main.bounds
+    return CGSize(width: screen.width * 0.94, height: screen.height)
+  }
+  
+  func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, insetForSectionAt section: Int) -> UIEdgeInsets {
+    return UIEdgeInsets(top: 10, left: 10, bottom: 10, right: 10)
+  }
   
 //  func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, minimumInteritemSpacingForSectionAt section: Int) -> CGFloat {
 //    return 10.0
